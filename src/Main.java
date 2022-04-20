f28c7fd194c0a740e9ec08c84afcb6b69d017d7b
gemsvido <gemsvido@gmail.com>
2022-04-20 04:29:21 -0700

    re

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import java.io.IOException;
-
 public class Main {
 
 
     public static volatile String commit_message;
 
     public static guiWindow e = new guiWindow();
+
+    public static Boolean adding_started = false;
+
+    public static Boolean committing_started = false;
+
+    public static Boolean pushing_started = false;
 
 
     public static Boolean adding_done = false;
@@ -25,66 +29,80 @@ public class Main {
     public static void finish () throws Exception {
 
 
-
-
         Thread _add = new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-                e.button.setText("Adding...");
-                e.frame.pack();
-                e.frame.update(e.frame.getGraphics());
-                Process add = Runtime.getRuntime().exec("git add --all");
 
-                while (add.isAlive()) {
-                    Thread.sleep(1000);
-                }
+            if (!adding_started) {
 
-                adding_done = true;
+                try {
+                    e.button.setText("Adding...");
+                    e.frame.pack();
+                    e.frame.update(e.frame.getGraphics());
+                    Process add = Runtime.getRuntime().exec("git add --all");
 
+                    adding_done = true;
 
-
-            } catch (Exception ignored) {}
+                } catch (Exception ignored) {}
+            }
         });
 
-
         Thread _commit = new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-                e.button.setText("Committing...");
-                e.frame.pack(); e.frame.update(e.frame.getGraphics());
-                Process commit = Runtime.getRuntime().exec("git commit -m " + commit_message + "");
-                Thread.sleep(1000);
-                committing_done = true;
+
+            if (!committing_started) {
+
+                try {
+                    e.button.setText("Committing...");
+                    e.frame.pack();
+                    e.frame.update(e.frame.getGraphics());
+                    Process commit = Runtime.getRuntime().exec("git commit -m " + commit_message + "");
+                    committing_done = true;
 
 
-            } catch (Exception ignored) {}
+                } catch (Exception ignored) {}
+            }
         });
 
         Thread _push = new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-                e.button.setText("Pushing...");
-                e.frame.pack();
-                e.frame.update(e.frame.getGraphics());
-                Process push = Runtime.getRuntime().exec("git push origin master");
-                Thread.sleep(3000);
 
-                pushing_done = true;
+            if (!pushing_started) {
+                try {
+                    e.button.setText("Pushing...");
+                    e.frame.pack();
+                    e.frame.update(e.frame.getGraphics());
+                    Process push = Runtime.getRuntime().exec("git push origin master");
 
-            } catch (Exception ignored) {}
+                    pushing_done = true;
+
+                } catch (Exception ignored) {}
+            }
         });
 
 
+        while (true) {
 
-        _add.start();
+            if (!adding_done && !committing_done && !pushing_done) {
+                _add.start();
+            }
+
+
+            if (adding_done && !committing_done && !pushing_done) {
+                _commit.start();
+            }
+
+
+            if (adding_done && committing_done && !pushing_done) {
+                _push.start();
+            }
 
 
 
-        _commit.start();
 
+            if (adding_done && committing_done && pushing_done) {
+                break;
+            }
 
-        _push.start();
-        Thread.sleep(2000);
+            Thread.sleep(1000);
+    }
+
 
 
 
