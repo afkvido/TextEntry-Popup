cc3581649b26bf14a42b9083fe0e22592595dcb1
gemsvido <gemsvido@gmail.com>
2022-04-20 04:39:14 -0700

    com

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,22 @@ public class Main {
 
         e.button.setText("Adding...");
         e.frame.getContentPane().update(e.frame.getGraphics());
-        Runtime.getRuntime().exec("git add --all");
+        Process a = Runtime.getRuntime().exec("git add --all");
+        a.waitFor();
 
         e.button.setText("Committing...");
         e.frame.getContentPane().update(e.frame.getGraphics());
-        Runtime.getRuntime().exec("git commit -m " + commit_message + "");
+        Process c = Runtime.getRuntime().exec("git commit -m " + commit_message + "");
+        c.waitFor();
 
         e.button.setText("Pushing...");
         e.frame.getContentPane().update(e.frame.getGraphics());
-        Runtime.getRuntime().exec("git push origin master");
+        Process p = Runtime.getRuntime().exec("git push origin master");
+        p.waitFor();
 
-
+        e.button.setText("Done!");
+        e.frame.getContentPane().update(e.frame.getGraphics());
+        Thread.sleep(200);
+        System.exit(0);
     }
 }
